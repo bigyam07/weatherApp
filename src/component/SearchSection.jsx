c956d7c8806aeaaa842f101794332babfdb456d1
bigyam07 <bigyamkarmacharya@gmail.com>
2025-09-07 11:50:33 +0545

    Fixed naming error

--- src/component/SearchSection.jsx
+++ src/component/SearchSection.jsx
@@ -4,10 +4,10 @@ const SearchSection = () => {
         <>
             <div className={styles["search-container"]}>
                 <div className={styles["search-section"]}>
-                    <div className={styles["search-icon"]}><label for="input-text"><i class="fa-solid fa-magnifying-glass"></i></label></div>
+                    <div className={styles["search-icon"]}><label htmlFor="input-text"><i className="fa-solid fa-magnifying-glass"></i></label></div>
                     <input type='text' placeholder='Enter a city name' id="input-text" />
                 </div>
-                <button className={styles['location-btn']}><i class="fa-solid fa-location-crosshairs"></i></button>
+                <button className={styles['location-btn']}><i className="fa-solid fa-location-crosshairs"></i></button>
             </div>
         </>
     )
